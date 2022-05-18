c71ce808989be648c3599e326d5e1fca7db8997d
GALENS <260827400@qq.com>
2022-05-19 07:26:45 +0800

    更新

---
+++ ArticleSourceCode/DgnTools/DgnToolFunctionTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bentley.DgnPlatformNET;
+
+namespace ArticleSourceCode.DgnTools
+{
+    class DgnToolFunctionTest : Bentley.DgnPlatformNET.DgnTool
+    {
+        protected override void ExitTool()
+        {
+            
+        }
+
+        protected override bool OnDataButton(DgnButtonEvent ev)
+        {
+            
+        }
+
+        protected override bool OnResetButton(DgnButtonEvent ev)
+        {
+            
+        }
+    }
+}
